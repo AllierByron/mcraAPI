428637b5854a8bcae6ca11270aa7ad2e33c35d08
Byron <212220367@soy.utj.edu.mx>
2023-08-08 09:37:10 -0600

    Nuevo SSL

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //cors
 app.use(cors({
-  origin:"https://localhost:443",
+  origin:"http://localhost:4200",
   methods: "POST, GET, DELETE, PUT",
   preflightContinue: false,
   optionsSuccessStatus: 204
