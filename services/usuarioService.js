923848b9a2225d39b58e53134b55990b724ef60a
Byron <212220367@soy.utj.edu.mx>
2023-08-08 09:48:29 -0600

    Token Apagado

--- services/usuarioService.js
+++ services/usuarioService.js
@@ -11,15 +11,18 @@ exports.crearToken = (user)=>{
 
 //funcion conocida como middleware, verificacion de autenticidad del token
 exports.verificarHeaderToken = (req, res, next)=>{
-    const authHeader = req.headers['authorization'];//del la solicitud http obtenemos el header especificado
-    const token = authHeader && authHeader.split(' ')[1];//la palabra bearer viene en el header, separamos el token
-    if(token == null) return res.status(401).send('No se ha enviado token');//si la operacion anterior falla, se envia
+    if(false){
+        const authHeader = req.headers['authorization'];//del la solicitud http obtenemos el header especificado
+        const token = authHeader && authHeader.split(' ')[1];//la palabra bearer viene en el header, separamos el token
+        if(token == null) return res.status(401).send('No se ha enviado token');//si la operacion anterior falla, se envia
 
-    //si el token es contenido ahora se verifica que sea valido
-    jwt.verify(token, secret, (err, usuario)=>{
-        if(err) return res.status(403).send({err});//si existe algun error este se retorna
-        // console.log(usuario);
-        req.usuario = usuario;//'usuario' es el PAYLOAD o el contenido del token
-        next();//se inicia el siguente callback
-    });
+        //si el token es contenido ahora se verifica que sea valido
+        jwt.verify(token, secret, (err, usuario)=>{
+            if(err) return res.status(403).send({err});//si existe algun error este se retorna
+            // console.log(usuario);
+            req.usuario = usuario;//'usuario' es el PAYLOAD o el contenido del token
+            next();//se inicia el siguente callback
+        });
+    }
+    next();
 }
